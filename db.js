93ed460553c51b13863b9da00632ac5a39bf7738
unknown <nproger781@gmail.com>
2023-02-01 08:52:16 +0700

    запись в файд json

--- db.js
+++ db.js
@@ -1,12 +1,31 @@
 const mysql = require('mysql');
-
 const config = {
     host: 'localhost',
     user: 'root',
     password: 'root',
-    database: 'Website-Carabin'
-}
+    database: 'website_carabin'
+};
 
 const pool = mysql.createPool(config);
 
-module.exports = pool;
+function boolean_el(){
+    let num = Math.floor(Math.random()*2);
+    if(num == 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function send_data(id, id_cat, name_product, product_url, price, about, produser){
+    pool.query(`INSERT INTO products(id, id_cat, name_product, product_url, popular, new, price, about, produser) VALUES ('${id}',${id_cat},'${name_product}','${product_url}',${boolean_el()},${boolean_el()},${price},'${about}','${produser}')`, (err, res) => {
+        if(err){
+            console.log(err);
+        } else {
+            console.log('send');
+        }
+    });
+}
+
+module.exports = boolean_el();
+module.exports = send_data;
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const { JSDOM } = require('jsdom');
+const jsonfile = require('jsonfile');
+let boolean_el = require('./db.js');
+const send_data = require('./db.js');
 const data_odezhda = [];
 const data_obuv = [];
+const https = 'https://trofey.ru/catalog/';
 
 class Product{
-    constructor(name, producer, price, url, link){
+    constructor(id, name, producer, price, url, link){
+        this.id = id,
         this.name = name,
         this.producer = producer,
         this.price = price,
@@ -12,7 +17,10 @@ class Product{
     }
 }
 
-const https = 'https://trofey.ru/catalog/';
+function create_json(arr){
+    jsonfile.writeFile('data.json', arr);
+    console.log('write');
+}
 
 async function get_more_info(arr){
     for (const el of arr) {
@@ -21,7 +29,7 @@ async function get_more_info(arr){
         let prod_descript = doc.querySelector('div.text>p').innerHTML;
         el.about = prod_descript;
     }
-    console.log(JSON.stringify(arr, null, ' '));
+    create_json(arr);
 }
 
 async function get_data(param, arr){
@@ -29,13 +37,14 @@ async function get_data(param, arr){
         const dom = await JSDOM.fromURL(`${https}${param}`);
         const doc = dom.window.document;
         let products = doc.querySelectorAll('div.item-e');
-        for (let i = 23; i < 33; i++) {
+        for (let i = 23; i < 25; i++) {
+            let prod_id = products[i].querySelector('div.text>p>a').id;
             let prod_name = products[i].querySelector('div.text>p>a>span').innerHTML;
             let prod_producer = products[i].querySelector('div.text>div.item-list-title>span').textContent;
-            let prod_price = products[i].querySelector('div.text>div.item-list-title span.price').textContent;
+            let prod_price = products[i].querySelector('div.text>div.item-list-title span.price').innerHTML;
             let prod_url = products[i].querySelector('picture>img').src;
             let link = products[i].querySelector('div.text>p>a').href;
-            let el_prod = new Product(prod_name, prod_producer, prod_price, prod_url, link);
+            let el_prod = new Product(prod_id, prod_name, prod_producer, prod_price, prod_url, link);
             arr.push(el_prod);
         }
     } catch (e) {
